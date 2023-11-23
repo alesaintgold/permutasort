86d8bd5bbb16f01535cd60740149a2de2e2e2cc7
Ale Santoro <alexi.santoro@gmail.com>
2023-11-23 15:08:46 +0100

    combination of every number of operators

--- src/operators.py
+++ src/operators.py
@@ -3,6 +3,7 @@ from src.algorithms.queuesort import Q
 from src.algorithms.stacksort import S 
 from src.algorithms.composition import compositionOf
 
+#if implementing new operators, add them to the index
 algorithmsIndex = {
  	"B": B,
  	"Q": Q,
@@ -10,15 +11,11 @@ algorithmsIndex = {
 }
 
 def getOperator(key):
-	if len(key) not in (1,2):
-		print("ERROR: unknown operator\n\npython traceback:\n")	
-		exit()
-	elif len(key) == 2:
-		return compositionOf(getOperator(key[0]), getOperator(key[1]))
-	elif len(key) == 1:
+	if len(key)==1:
 		if key not in algorithmsIndex:
-			print("ERROR: unknown operator\n\npython traceback:\n")
+			print("ERROR: unsupported operator " + key)
+			exit()
 		else:
 			return algorithmsIndex[key]
 	else:
-		print ("ERROR: unexpected error\n\npython traceback:\n")
+		return compositionOf(getOperator(key[0:1]), getOperator(key[1:]))
