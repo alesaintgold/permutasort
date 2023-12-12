d7ec13a4819cde77e5f305c2ffe8b86a978a2ba7
Ale Santoro <alexi.santoro@gmail.com>
2023-12-13 00:01:12 +0100

    small changes

--- permutasort.py
+++ permutasort.py
@@ -28,7 +28,7 @@ class selectorPermutations:
 				self.__outcomes.append(op_P_)
 			
 			# adding permutations to the right list
-			if isPermutationSorted(op_P_):
+			if isIdentityPermutation(op_P_):
 				self.__sortable.append(P)
 			else:
 				self.__unsortable.append(P)
